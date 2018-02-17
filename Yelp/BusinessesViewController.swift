afe461a37bd74103f52f4f205511a75b0b8ce13e
DiegoAMedina <dimedina@csumb.edu>
2018-02-16 22:48:23 -0800

    finished yelp without optionals

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,25 +8,46 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate{
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    var searchBar = UISearchBar()
     var businesses: [Business]!
-    
+    var filteredBusinesses : [Business]!
+    var isSearching = false
+    var isMoreDataLoading = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
-            self.businesses = businesses
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
+                self.businesses = businesses
+                self.tableView.reloadData()
+            
+                if let businesses = businesses {
+                    for business in businesses {
+                        print(business.name!)
+                        print(business.address!)
+                    }
                 }
             }
-            
-            }
         )
+        
+        // search bar
+        filteredBusinesses = businesses
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        
         
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
@@ -45,6 +66,80 @@ class BusinessesViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        /*if businesses != nil {
+            return businesses!.count
+        }*/
+        if isSearching {
+            return filteredBusinesses!.count
+        }
+        else if businesses != nil{
+            return businesses!.count
+            
+        }
+        else{
+            return 0;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath as IndexPath) as! BusinessCell
+        
+        
+        if isSearching{
+            cell.business = filteredBusinesses[indexPath.row]
+        }
+        else{
+            cell.business = businesses[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == ""{
+            
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        }
+        else{
+            
+            isSearching = true
+            filteredBusinesses = businesses.filter(
+                { ($0.name?.lowercased().contains( searchBar.text!.lowercased()))! })
+            tableView.reloadData()
+        }
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        if !isMoreDataLoading{
+            
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging){
+                isMoreDataLoading = true
+            }
+        }
+        
+    }
+    
+    
+    func loadMoreData(){
+        //TODO
+
+    }
+    
+    
+    
+    
+    
+    
     
     /*
      // MARK: - Navigation
